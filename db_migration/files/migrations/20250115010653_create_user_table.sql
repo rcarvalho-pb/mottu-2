8f4ceef2346f4200f2677118775ace76d95bbc75
Ramon Carvalho <rcarvalho.pb@gmail.com>
2025-01-15 07:39:21 -0300

    feat: create db migrations

---
+++ db_migration/files/migrations/20250115010653_create_user_table.sql
@@ -0,0 +1,26 @@
+-- +goose Up
+-- +goose StatementBegin
+SELECT 'up SQL query';
+-- +goose StatementEnd
+CREATE TABLE IF NOT EXISTS tb_users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL,
+    password TEXT NOT NULL,
+    role INTEGER DEFAULT 1,
+    name TEXT NOT NULL,
+    birth_date TIMESTAMP NOT NULL,  -- Alterado para DATE
+    cnpj TEXT NOT NULL,
+    cnh TEXT NOT NULL,
+    cnh_type TEXT NOT NULL,
+    cnh_file_path TEXT NOT NULL DEFAULT '',
+    active_location BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,  -- Isso precisa de um trigger para ser atualizado automaticamente
+    active BOOLEAN DEFAULT TRUE,
+    CONSTRAINT unique_user_cnpj UNIQUE (cnpj),
+    CONSTRAINT unique_user_cnh UNIQUE (cnh)
+);
+-- +goose Down
+-- +goose StatementBegin
+SELECT 'down SQL query';
+-- +goose StatementEnd
---
+++ db_migration/go.mod
@@ -0,0 +1,15 @@
+module github.com/rcarvalho-pb/mottu-db_migration
+
+go 1.23.4
+
+require (
+	github.com/mattn/go-sqlite3 v1.14.24
+	github.com/pressly/goose/v3 v3.24.1
+)
+
+require (
+	github.com/mfridman/interpolate v0.0.2 // indirect
+	github.com/sethvargo/go-retry v0.3.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/sync v0.10.0 // indirect
+)
